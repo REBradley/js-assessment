123a4bc0ede6b32a387f6742f42c38f0df7b1b2e
REBradley <reb337@cornell.edu>
2016-11-08 15:55:52 -0800

    working on async

--- app/count.js
+++ app/count.js
@@ -2,6 +2,18 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.countAnswers = {
   count: function (start, end) {
-
+      var has = function() {
+	    var i = start;
+            var step = function(){
+		     console.log(1);
+	             i++;
+		     if (i > end) {
+		         return clearInterval(a);
+		     }
+                 };
+	    a = setInterval(step, 100);
+            var cancel = function(){clearInterval(a);};
+      }
+  return setTimeout(has, 100);
   }
 };
--- app/modules.js
+++ app/modules.js
@@ -2,6 +2,13 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.modulesAnswers = {
   createModule: function(str1, str2) {
-
+      var container = {
+          sayIt : function () {
+		      return (this.greeting + ', ' + this.name)
+		  },
+          name : str2,
+          greeting : str1,
+      }
+      return container;
   }
 };
